5394b1839e59feade1d71b743e7f920b0d105bd6
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-05-21 11:49:48 +0530

    feat[getNearRestaurants]:get location and fetch restaurants under 5km

--- Routes/restaurantRouter.js
+++ Routes/restaurantRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Define routes
 router.post('/', restaurantController.createRestaurant);
 router.get('/', restaurantController.getAllApprovedRestaurants);
-router.get('/:id', restaurantController.getRestaurantById);
+router.get('/getId/:id', restaurantController.getRestaurantById);
 router.patch('/:id', restaurantController.updateRestaurant);
 router.delete('/:id', restaurantController.deleteRestaurant);
 
